da67b5ee9e391c4f67fcf7b37b0fd1b4e0c2d98d
Rodrigo Nakao <rodrigonakao@gmail.com>
2019-11-03 22:21:45 -0200

    Lesson 3 - configuração firebase

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,32 +1,60 @@
-import { computed, observer } from '@ember/object';
+// import { computed, observer } from '@ember/object';
+// import Controller from '@ember/controller';
+// import { empty } from '@ember/object/computed';
+// import { match, not } from '@ember/object/computed';
+
+// export default Controller.extend({
+   
+//   emailAddress: '',
+
+
+//   isValid: match('emailAddress', /^.+@.+\..+$/),
+//   isDisabled: not('isValid'),
+//   headerMessage: 'Comming Soon variable',
+
+//   actions: {
+
+//     saveInvitation() {
+//       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+//       this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+//       this.set('emailAddress', '');
+//     }
+//   },
+
+//   actualEmailAddress: computed('emailAddress', function() {
+//     console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+//   }),
+
+//   emailAddressChanged: observer('emailAddress', function() {
+//     console.log('observer is called', this.get('emailAddress'));
+//   })
+
+// });
+
+//lesson 3
 import Controller from '@ember/controller';
-import { empty } from '@ember/object/computed';
 import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
-   
-  emailAddress: '',
 
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
+  emailAddress: '',
 
   isValid: match('emailAddress', /^.+@.+\..+$/),
   isDisabled: not('isValid'),
-  headerMessage: 'Comming Soon variable',
 
   actions: {
 
     saveInvitation() {
-      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+      newInvitation.save();
+
+      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
     }
-  },
+  }
 
-  actualEmailAddress: computed('emailAddress', function() {
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
-
-  emailAddressChanged: observer('emailAddress', function() {
-    console.log('observer is called', this.get('emailAddress'));
-  })
-
-});
+}); 
